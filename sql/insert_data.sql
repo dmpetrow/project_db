79ad86981086deef4a1651da16d720c28bb82ab6
dmpetrow <dedu.03@mail.ru>
2024-12-12 00:24:26 +0300

    Feat: Добавление скрипта для добавления данных

---
+++ sql/insert_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO constellations (name, latin_name, area_square_degrees)
+VALUES
+('Андромеда', 'Andromeda', 722.3),
+('Большая Медведица', 'Ursa Major', 1279.7),
+('Кассиопея', 'Cassiopeia', 598.4),
+('Орион', 'Orion', 594.1),
+('Малая Медведица', 'Ursa Minor', 255.9),
+('Дева', 'Virgo', 1294.4),
+('Геркулес', 'Hercules', 1225.1),
+('Дракон', 'Draco', 1082.95),
+('Волопас', 'Boötes', 906.8),
+('Северная Корона', 'Corona Borealis', 178.7);
